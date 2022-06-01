638560445de1b5fe401e54fa71d0e03f9854de51
Parth Patel <parth.p@brilworks.com>
2022-06-01 12:01:27 +0530

    Deploy on heroku

--- social_login/urls.py
+++ social_login/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include # new
-
+from django.conf import settings
+from django.conf.urls.static import static
 from app.views import Home # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")), # new
      path("", Home.as_view(), name="home"), # new
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
